68e629165e39da10f246da1112d44f40f63b5a3d
gyuheeb <gyuhee4529@naver.com>
2023-01-30 19:20:20 +0900

    my site03 #2

---
+++ mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -0,0 +1,268 @@
+package com.douzone.mysite.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.douzone.mysite.vo.UserVo;
+
+@Repository
+public class UserRepository {
+	// FINDALL
+	public List<UserVo> findAll() {
+		List<UserVo> result = new ArrayList<>();
+	
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql ="select * from user";
+			pstmt = conn.prepareStatement(sql);
+
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				UserVo vo = new UserVo();
+				vo.setNo(rs.getLong(1));
+				vo.setName(rs.getString(2));
+				vo.setEmail(rs.getString(3));
+				vo.setPassword(rs.getString(4));
+				vo.setGender(rs.getString(5));
+				vo.setJoinDate(rs.getString(6));
+				
+				result.add(vo);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+	
+	//INSERT
+	public void insert(UserVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql = "insert into user values(null, ?, ?, password(?), ?, now())";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getEmail());
+			pstmt.setString(3, vo.getPassword());
+			pstmt.setString(4, vo.getGender());
+
+		
+			
+			pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+//LOGIN
+	public UserVo findByEmailAndPassword(UserVo vo) {
+		UserVo result =null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs =null;
+		try {
+			conn = getConnection();
+			
+			String sql = "select no,name from user where email= ? and password = password(?)";
+			pstmt = conn.prepareStatement(sql);
+			
+			
+			pstmt.setString(1, vo.getEmail());
+			pstmt.setString(2, vo.getPassword());
+			
+		
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				result = new UserVo();
+				
+				Long no =rs.getLong(1);
+				String name =rs.getString(2);
+				
+				result.setNo(no);
+				result.setName(name);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+				
+		return result;
+	}
+	// 회원정보 수정 FINDBYNO
+	public UserVo findByNo(Long no) {
+		UserVo result =null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs =null;
+		try {
+			conn = getConnection();
+			
+			String sql = "select no,name,email,password,gender from user where no = ?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, no);
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				result = new UserVo();
+				
+				result.setNo(rs.getLong(1));
+				result.setName(rs.getString(2));
+				result.setEmail(rs.getString(3));
+				result.setPassword(rs.getString(4));
+				result.setGender(rs.getString(5));
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+				
+		return result;
+	}
+	// 회원정보 수정 Update
+	public void update(UserVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			if("".equals(vo.getPassword())) {
+				conn = getConnection();
+				
+				String sql = "update user set name = ?, gender=? where no=?";
+				pstmt = conn.prepareStatement(sql);
+				
+				pstmt.setString(1, vo.getName());
+				pstmt.setString(2, vo.getGender());
+				pstmt.setLong(3, vo.getNo());
+		
+			
+				pstmt.executeUpdate();
+				
+			}else {
+				conn = getConnection();
+				
+				String sql = "update user set name = ?,  password =password(?), gender=? where no=?";
+				pstmt = conn.prepareStatement(sql);
+				
+				pstmt.setString(1, vo.getName());
+				pstmt.setString(2, vo.getPassword());
+				pstmt.setString(3, vo.getGender());
+				pstmt.setLong(4, vo.getNo());
+			
+				
+				pstmt.executeUpdate();
+				
+			}
+			
+			
+			
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private Connection getConnection() throws SQLException {
+	Connection conn = null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.10.118:3307/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		}
+		
+		return conn;
+	}
+
+	
+
+	
+
+	
+}
+	
+
