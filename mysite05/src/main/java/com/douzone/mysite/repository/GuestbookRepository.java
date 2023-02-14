bbe5ba09c2c05d620c0ed8a800074178f7c10850
gyuheeb <gyuhee4529@naver.com>
2023-02-14 11:17:17 +0900

    mysite05: ServletContainerInitializer

---
+++ mysite05/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -0,0 +1,41 @@
+package com.douzone.mysite.repository;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.douzone.mysite.vo.GuestbookVo;
+
+@Repository
+public class GuestbookRepository {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	// FINDALL
+	public List<GuestbookVo> findAll() {
+		
+		List<GuestbookVo> list = sqlSession.selectList("guestbook.findAll");
+		return list;
+	}
+	
+	//INSERT
+	public void insert(GuestbookVo vo) {
+		sqlSession.insert("guestbook.insert",vo);
+	}
+	
+	//DELETE
+	public void deleteByGuest(Long no,String password) {
+		
+		Map<String, Object> map = Map.of("no",no,"password",password);
+		sqlSession.delete("guestbook.deleteByGuest",map);
+		
+	}
+	
+
+	
+}
+	
+
