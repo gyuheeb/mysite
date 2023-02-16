2e0cfec06a4d73cf9f249b0a8e12fc8168d655f9
gyuheeb <gyuhee4529@naver.com>
2023-02-16 13:51:40 +0900

    mysite05 modify

--- mysite05/src/main/java/com/douzone/mysite/config/web/MvcConfig.java
+++ mysite05/src/main/java/com/douzone/mysite/config/web/MvcConfig.java
@@ -4,7 +4,6 @@ import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -13,7 +12,6 @@ import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
