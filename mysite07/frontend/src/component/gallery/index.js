f81c5c0dd6e1643464bdccfc355c9e3188db763a
gyuheeb <gyuhee4529@naver.com>
2023-03-21 13:22:26 +0900

    mysite07 #finish

--- mysite07/frontend/src/component/gallery/index.js
+++ mysite07/frontend/src/component/gallery/index.js
@@ -7,7 +7,7 @@ import styles from '../../assets/scss/component/gallery/Galery.scss';
 export default function Index() {
     const [imageList, setImageList] = useState([]);
 
-    useEffect(async () => {
+    const fetchList = async () => {
         try {
             const response = await fetch('/api/gallery', {
                 method: 'get',
@@ -29,6 +29,9 @@ export default function Index() {
         } catch (err) {
             console.error(err);
         }
+    }
+    useEffect(()=>{
+        fetchList();
     }, []);
 
     const notifyImage = {
